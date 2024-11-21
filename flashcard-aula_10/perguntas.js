c27e54140e0fd9ce74de8f790d6775d6efcbf9c8
Daniel162007 <144928536+Daniel162007@users.noreply.github.com>
2024-11-21 11:01:40 -0300

    Add files via upload

--- flashcard-aula_10/perguntas.js
+++ flashcard-aula_10/perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'day trade',
+    'O que é mini indice?',
+    'O mini-índice é uma modalidade de investimento que consiste em um minicontrato negociado na Bolsa de Valores do mercado futuro'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'day trade',
+    'o que é mini dólar?',
+    'O minidólar, operado pelo código WDO, corresponde a 20% do contrato futuro de dólar cheio, ou seja, o WDO é um minicontrato com vencimentos estabelecidos. Em termos mais simples, é um compromisso de compra ou venda da moeda quando chega determinada data'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'day trade',
+    'o que é mineração de bitcoin?',
+    'Minerar Bitcoin é o processo que registra e valida transações sem a necessidade de um controle central'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'day trade',
+    'o que é forex?',
+    'No Forex, negocia-se pares de moedas, como EUR/USD (euro/dólar), ou seja, ao vender uma moeda, você automaticamente compra outra'
 )
